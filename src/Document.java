cda2aecd53cdde0658b46fae3ef295bdc8290150
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-30 10:31:03 +0100

    Update 1

--- src/Document.java
+++ src/Document.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import composite.Element;
 
 public class Document {
+
+	private List<Element> elements;
+
+	public Document() {
+		this.elements = new ArrayList<Element>();
+	}
+
+	public void addElement(Element e) {
+		this.elements.add(e);
+	}
+
+	public Iterator<Element> getIterator() {
+		return this.elements.iterator();
+	}
 
 }
--- src/ElementFactory.java
+++ src/ElementFactory.java
@@ -1,4 +1,26 @@
+import composite.Element;
 
 public class ElementFactory {
+
+	private static ElementFactory instance = null;
+
+	private ElementFactory() {
+		initHashMap();
+	}
+
+	public static ElementFactory getInstance() {
+		if (instance == null)
+			instance = new ElementFactory();
+
+		return instance;
+	}
+
+	private void initHashMap() {
+
+	}
+
+	public Element createElement(String elementType) {
+
+	}
 
 }
