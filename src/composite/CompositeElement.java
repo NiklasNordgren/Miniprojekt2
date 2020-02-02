1729b57284ba6c9fe34b211e6b0d4308fb6b35f4
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-02-02 09:12:18 +0100

    Added index to addElement method

--- src/composite/CompositeElement.java
+++ src/composite/CompositeElement.java
@@ -4,27 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CompositeElement extends Element {
-	
+
 	public CompositeElement() {
 		this.elements = new ArrayList<Element>();
 	}
-	
+
 	@Override
 	public void print() {
-		for(Element e : elements)
+		for (Element e : elements)
 			System.out.println(e.text);
 	}
 
 	@Override
-	public void addElement(Element element) {
-		this.elements.add(element);
+	public void addElement(int index, Element element) {
+		if (!(index > elements.size()) && element != null)
+			this.elements.add(index, element);
 	}
-	
+
 	@Override
 	public List<Element> getElements() {
 		return this.elements;
 	}
-	
+
 	@Override
 	public void setText(String text) {
 	}
--- src/composite/Element.java
+++ src/composite/Element.java
@@ -3,14 +3,14 @@ package composite;
 import java.util.List;
 
 public abstract class Element {
-	
+
 	protected String text;
 	protected List<Element> elements;
 
 	public void print() {
 		System.out.println(text);
 	}
-	
+
 	public void setText(String text) {
 		this.text = text;
 	}
@@ -18,10 +18,10 @@ public abstract class Element {
 	public String getText() {
 		return this.text;
 	}
-	
-	public void addElement(Element element) {
+
+	public void addElement(int index, Element element) {
 	}
-	
+
 	public List<Element> getElements() {
 		return this.elements;
 	}
